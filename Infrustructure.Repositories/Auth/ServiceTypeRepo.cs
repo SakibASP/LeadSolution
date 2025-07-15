0e2e24d088e8e575eb8e3c00942900561ecfe968
SakibASP <sakibur.rahman.cse@gmail.com>
2025-07-15 23:24:55 +0600

    upd

--- Infrustructure.Repositories/Auth/ServiceTypeRepo.cs
+++ Infrustructure.Repositories/Auth/ServiceTypeRepo.cs
@@ -8,7 +8,8 @@ namespace Infrustructure.Repositories.Auth;
 public class ServiceTypeRepo(LeadContext context) : IServiceTypeRepo
 {
     private readonly LeadContext _context = context;
-    public async Task<IList<AspNetServiceTypes>> GetAspNetServiceTypesAsync() => await _context.AspNetServiceTypes
+    public async Task<IList<AspNetServiceTypes>> GetAspNetServiceTypesAsync() 
+        => await _context.AspNetServiceTypes
         .AsNoTracking()
         .Where(x => x.IsActive)
         .ToListAsync();
