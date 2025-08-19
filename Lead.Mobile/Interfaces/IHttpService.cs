2dbd794479ab237d8b821298ceb4833ba8a886d4
SakibASP <sakibur.rahman.cse@gmail.com>
2025-08-20 03:39:03 +0600

    mobile removed

--- Lead.Mobile/Interfaces/IHttpService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Lead.Mobile.Interfaces;
-
-public interface IHttpService
-{
-    void SetBearerToken(string token);
-    Task<T?> PostAsync<T>(string version, string endpoint, object data, Dictionary<string, string>? headers = null);
-    Task<T?> GetAsync<T>(string version, string endpoint, Dictionary<string, string>? queryParams = null);
-}
